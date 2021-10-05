1dd62a3da32aac47154bab4a204192707b8f0a2d
rakupro <rakuprog@gmail.com>
2021-10-05 15:54:02 +0900

    Initial Commit

---
+++ Shared/SwiftSpeakerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpeakerDemoApp.swift
+//  Shared
+//
+//  Created by XcodeDeveloper on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpeakerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
